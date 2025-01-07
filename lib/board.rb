0b108b2161c71fd43f945a6224396afb377b65c8
Fahad Ahmed <fahad838@gmail.com>
2025-01-07 11:28:42 +0530

    Create Knight Class for representing knights
    Knight class initializes a white and black knight.
    I have unicode to represents the correct knight
    diagram on console same method as pawns.
    
    I have also setup Board object to show knights on
    board by creating a knight_setup method.

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,13 @@
 require './lib/pieces/pawn'
+require './lib/pieces/knight'
 
 class Board
-  attr_accessor :board, :pawn
+  attr_accessor :board, :pawn, :knight
 
   def initialize
     @pawn = Pawn.new
+    @knight = Knight.new
     @board = Array.new(8) { Array.new(8, '.') }
-    # @board = Array.new(8) do |num|
-    #   Array.new(8) do
-    #     Pawn.new if [1, 6].include?(num)
-    #   end
-    # end
     @num_arr = (1..8).to_a
   end
 
@@ -19,6 +16,18 @@ class Board
       row.each_with_index do |_cell, col_index|
         @board[row_index][col_index] = pawn.pawn_white if row_index == 6
         @board[row_index][col_index] = pawn.pawn_black if row_index == 1
+      end
+    end
+  end
+
+  def knight_setup
+    @board.each_with_index do |row, row_index|
+      row.each_with_index do |_cell, col_index|
+        if row_index.zero? && [1, 6].include?(col_index)
+          @board[row_index][col_index] = knight.knight_black
+        elsif row_index == 7 && [1, 6].include?(col_index)
+          @board[row_index][col_index] = knight.knight_white
+        end
       end
     end
   end
@@ -38,4 +47,5 @@ end
 
 b = Board.new
 b.pawn_setup
+b.knight_setup
 b.show_board
---
+++ lib/pieces/knight.rb
@@ -0,0 +1,8 @@
+class Knight
+  attr_accessor :knight_white, :knight_black
+
+  def initialize
+    @knight_white = "\u2658"
+    @knight_black = "\u265E"
+  end
+end
