69f7e5be8b7469f44ece808a598ce73b7f33b105
Fahad Ahmed <fahad838@gmail.com>
2025-01-07 11:05:37 +0530

    Create a pawn class in pieces folder
    Pawn class initializes a pawn of white and black
    respectively of its unicode. it properly
    represents a pawn piece.
    
    I also arranges the pawn pieces on the chess board
    correctly replacing the '.' with the pawn_white or
    pawn_black respective of their position on chess.

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,26 @@
+require './lib/pieces/pawn'
+
 class Board
+  attr_accessor :board, :pawn
+
   def initialize
+    @pawn = Pawn.new
     @board = Array.new(8) { Array.new(8, '.') }
+    # @board = Array.new(8) do |num|
+    #   Array.new(8) do
+    #     Pawn.new if [1, 6].include?(num)
+    #   end
+    # end
     @num_arr = (1..8).to_a
+  end
+
+  def pawn_setup
+    @board.each_with_index do |row, row_index|
+      row.each_with_index do |_cell, col_index|
+        @board[row_index][col_index] = pawn.pawn_white if row_index == 6
+        @board[row_index][col_index] = pawn.pawn_black if row_index == 1
+      end
+    end
   end
 
   def show_board
@@ -18,4 +37,5 @@ class Board
 end
 
 b = Board.new
+b.pawn_setup
 b.show_board
---
+++ lib/pieces/pawn.rb
@@ -0,0 +1,10 @@
+class Pawn
+  attr_accessor :pawn_white, :pawn_black
+
+  def initialize
+    @pawn_white = "\u2659"
+    @pawn_black = "\u265F"
+  end
+end
+
+
