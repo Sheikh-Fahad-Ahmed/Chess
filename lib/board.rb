929cea9fc7dc412134d9e8c9e12ab8c018b6eab4
Fahad Ahmed <fahad838@gmail.com>
2025-01-07 11:44:54 +0530

    Create Rook class that represents a rook
    The Rook class initializes a white and black rook.
    Same as last time i have used unicode to represent
    them.
    
    The board class then places them on the board
    relative to their position on chess. The method
    is called rook_setup.

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,12 @@
 require './lib/pieces/pawn'
 require './lib/pieces/knight'
+require './lib/pieces/rook'
 
 class Board
-  attr_accessor :board, :pawn, :knight
+  attr_accessor :board, :pawn, :knight, :rook
 
   def initialize
+    @rook = Rook.new
     @pawn = Pawn.new
     @knight = Knight.new
     @board = Array.new(8) { Array.new(8, '.') }
@@ -32,6 +34,18 @@ class Board
     end
   end
 
+  def rook_setup
+    @board.each_with_index do |row, row_index|
+      row.each_with_index do |_cell, col_index|
+        if row_index.zero? && [0, 7].include?(col_index)
+          @board[row_index][col_index] = rook.rook_black
+        elsif row_index == 7 && [0, 7].include?(col_index)
+          @board[row_index][col_index] = rook.rook_white
+        end
+      end
+    end
+  end
+
   def show_board
     print '  '
     ('a'..'h').each { |i| print "#{i} " }
@@ -48,4 +62,5 @@ end
 b = Board.new
 b.pawn_setup
 b.knight_setup
+b.rook_setup
 b.show_board
---
+++ lib/pieces/rook.rb
@@ -0,0 +1,8 @@
+class Rook
+  attr_accessor :rook_white, :rook_black
+
+  def initialize
+    @rook_white = "\u2656"
+    @rook_black = "\u265C"
+  end
+end
